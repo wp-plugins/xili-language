22f326a86b18b1c695b51fed9c3ae722498e5ebe
michelwppi <michelwppi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-02 10:06:38 +0000

    XILI-LANGUAGE : 2.9.10 as current version - see change log
    git-svn-id: https://plugins.svn.wordpress.org/xili-language/tags/2.9.10@797634 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/nav-menu.dev.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function($) { 
+	$('#update-nav-menu').bind('click', function(e) {
+		//alert ('inseert');
+		if ( e.target && e.target.className && -1 != e.target.className.indexOf('item-edit')) {
+			var inputclass = '';
+			var inputvalue = '';
+			//var inputvalue2 = ''; 
+			$("input[value='"+xili_data.strings[0]+"'][type=text]").parent().parent().parent().each( function(){
+				$(this).children('.description-thin,.field-url,.field-link-target,.field-description').each( function(){
+					
+					$(this).children('label').each( function(){
+						//$(this).attr("style", "border:yellow solid 1px") ;
+						if ( !$(this).parent().hasClass("field-link-target") ) { $(this).css("visibility", "hidden") } ;  // attr disabled pb avec remove...
+						$(this).children('input').each( function(){
+							if ( $(this).hasClass("edit-menu-item-attr-title") ) {
+								inputvalue2 = $(this).attr("value") ;
+							}
+						});
+					});
+					
+					//$(this).attr("style", "border:red solid 1px") ;
+				});
+				
+				
+				p = $('<p>').attr('class', 'description topo').html( '<strong>' + xili_data.strings[1] + '</strong><br />' + inputvalue2 + '<br />' + xili_data.strings[2] );
+				
+				if ( !$(this).children('p').hasClass('topo') )
+				 { $(this).prepend(p); }
+				
+			});
+			
+			$("input[value='"+xili_data.strings[3]+"'][type=text]").parent().parent().parent().each( function(){
+				$(this).children('.description-thin,.field-url,.field-link-target,.field-description').each( function(){
+					
+					$(this).children('label').each( function(){
+						//$(this).attr("style", "border:yellow solid 1px") ;
+						if ( !$(this).parent().hasClass("field-link-target") ) { $(this).css("visibility", "hidden") } ;  // attr disabled pb avec remove...
+						$(this).children('input').each( function(){
+							if ( $(this).hasClass("edit-menu-item-attr-title") ) {
+								inputvalue2 = $(this).attr("value") ;
+							}
+						});
+					});
+					
+					//$(this).attr("style", "border:red solid 1px") ;
+				});
+				
+				
+				p = $('<p>').attr('class', 'description topo').html( '<strong>' + xili_data.strings[4] + '</strong><br /><br />' + inputvalue2 + '<br /><br />' + xili_data.strings[5] );
+				
+				if ( !$(this).children('p').hasClass('topo') )
+				 { $(this).prepend(p); }
+				
+			});
+		}
+	});
+});
